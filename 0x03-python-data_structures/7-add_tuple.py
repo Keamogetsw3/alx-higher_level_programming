49eeaf4e288134042224dc0cc8b8740875518ff7
keamogetswe3 <keamogetswegp@gmail.com>
2023-08-14 13:03:03 -0700

    adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
-    added_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return added_tuple
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    results = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return results
