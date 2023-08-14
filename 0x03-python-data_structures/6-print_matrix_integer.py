ac068f22def15bbb87a7f83a75afeda62860e8e0
keamogetswe3 <keamogetswegp@gmail.com>
2023-08-14 12:24:45 -0700

    fies

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
-            print("{:d}".format(column), end=" ")
+            print("".join("{:d}".format(column), end=" "))
 
         else:
             print()
