a38660e1902df22b4d9cf9c3eb8ea4b25d52719d
keamogetswe3 <keamogetswegp@gmail.com>
2023-08-14 10:59:15 -0700

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,27 @@
-#!/usr/bin/python3
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+/*
+* print_python_list_info - 
+* @p:
+*
+* Return:
+*/
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t index;
+    Py_ssize_t list_size;
+    PyObject *item;
+
+    list_size = PyList_Size(p)
+
+    printf("[*] Size of the Python List = %ld\n",size)
+    printf("[*] Allocated = %ld\n",((PyListObject *)p)->allocated);
+
+    for (index = 0; index < size; index++)
+    {
+        item = PyList_GetItem(p, index);
+        printf("Element %ld: %s\n", index, Py_TYPE(item)->tp_name);
+    }
+}
