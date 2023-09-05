fdb8f03d5e3f04eae8b1b374f7cb6b56bf692706
Keamogetsw3 <Keamogetswegp@gmail.com>
2023-09-05 12:58:29 -0700

    FILES CREATED AND ADDEDED

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+Defines a 2-matrix_divided module.
+"""
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements in the matrix by div.
+
+    Args:
+        matrix (list of lists): A matrix of integers or floats.
+        div (int or float): The divisor.
+
+    Returns:
+        list of lists: A new matrix with elements divided by div, rounded to 2 decimal places.
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    size = None
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in l] for l in matrix]
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Defines a single function that prints a squaree
+"""
+
+
+def print_square(size):
+    """
+    Prints a square made of "#" characters with each side having a length equal to 'size'.
+
+    args:
+        size (param): An integer specifying the length of each side of the square.
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size > 0:
+        print(("#" * size + "\n") * size, end="")
