cf41392797af3511f44cf4693c7ed49fbc094eae
Keamogetsw3 <Keamogetswegp@gmail.com>
2023-09-11 10:33:14 -0700

    Write a class MyList that inherits from list:

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" 
+Defines a class Mylist that inherits from 'list'
+"""
+
+
+class MyList(list):
+    """ 
+    Define a method to print the list elements in sorted order.
+    """
+
+    def print_sorted(self):
+        """ Sort the list in ascending order and print """
+        print(sorted(self))
