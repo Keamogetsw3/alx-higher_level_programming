b6abbbe3d0f359599c8728f8a94a09999ac5514d
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-09-12 18:43:42 +0200

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''
+"""
 Defines a function to append a string to a text file.
-'''
+"""
 
 
 def append_to_file(filename="", text=""):
-    '''
+    """
     Appends a string at the end of a text file (UTF8).
     Args:
        filename (str): The file to which text will be appended.
@@ -13,6 +13,6 @@ def append_to_file(filename="", text=""):
 
        Returns:
        int: The number of characters appended.
-    '''
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
