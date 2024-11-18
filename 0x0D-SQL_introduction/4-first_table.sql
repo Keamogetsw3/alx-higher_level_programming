7c3df1b7c102a8871b27d79fc0c477454c6e44da
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-18 15:00:45 +0200

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,7 +4,7 @@
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
-CREATE TABLE IF NOT EXIST first_table (
-       id INT
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,
        name VARCHAR(256)
 );
