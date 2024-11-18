626c4360341c59afc5659a60c4e6e09694f655ad
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-18 21:32:49 +0200

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- The database name will be passed as an argument of the mysql command
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
