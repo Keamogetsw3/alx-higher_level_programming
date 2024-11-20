2efc9a274f318c2d47fcc61c03c35c43bd96ff88
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-20 22:21:10 +0200

    Update 102-rating_shows.sql: Fixed syntax

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,8 +4,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT `title`, SUM(`tv_show_ratings`.`rate`) 'rating'
+SELECT `title`, SUM(`tv_show_ratings`.`rate`) AS `rating`
 FROM `tv_shows`
 LEFT JOIN `tv_show_ratings` ON `tv_show_ratings`.`show_id` = `tv_shows`.`id`
-GROUP BY `title
+GROUP BY `title`
 ORDER BY `rating` DESC;
