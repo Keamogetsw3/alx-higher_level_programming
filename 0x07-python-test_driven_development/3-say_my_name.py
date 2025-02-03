177a21d496c465bf6dcb141fee8dc2b39faefcc6
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-02-04 00:19:19 +0200

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-""" Defines a function for printing a name """
+"""
+This module provides a function to print a person's full name.
+
+The `say_my_name` function takes a first name and an optional last name 
+and prints a formatted message.
+"""
 
 
-def print_name(first_name, last_name=""):
-    """ Prints a name.
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first_name> <last_name>".
 
     Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
+        first_name (str): The first name to be printed.
+        last_name (str, optional): The last name to be printed. Defaults to an 
+            empty string.
+
     Raises:
-        TypeError: If either `first_name` or `last_name` are not strings.
+        TypeError: If `first_name` or `last_name` is not a string.
     """
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    print("My name is", first_name, last_name)
